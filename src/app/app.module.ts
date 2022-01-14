6e5d7a008c0b57b27342317ffb5d61e59fb26df6
james-muriithi <muriithijames556@gmail.com>
2022-01-14 22:48:35 +0300

    added user details component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    UserDetailsComponent
   ],
   imports: [
     BrowserModule,
